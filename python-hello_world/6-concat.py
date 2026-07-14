bd978f8a1837eeaee61684cdd6dc927699f97685
sanol-1 <sanol-1>
2026-07-14 03:06:50 -0700

    alu-higher_level_programming

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
+str1 = "Holberton"
 str2 = "School"
-message = "{} Holberton {}!".format(str1, str2)
-print("{}".format(message))
+str1 = f"{str1} {str2}"
+print(f"Welcome to {str1}!")
